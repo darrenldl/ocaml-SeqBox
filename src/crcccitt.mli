d767b3cd2483a10691e5a5271d7ab33c39ede6e7
Darren Ldl <dldldev@yahoo.com>
2017-06-27 22:46:26 +1000

    Added interface file crcccitt.mli

---
+++ src/crcccitt.mli
@@ -0,0 +1,15 @@
+(* The implementation code is translated from libcrc
+ *  URL : https://github.com/lammertb/libcrc (retrieved on 2017-06-27)
+ * The files used as referenced are:  
+ *  crcccitt.c
+ *  checksum.h
+ *)
+open Stdint
+
+val crc_ccitt_generic : input:bytes -> start_val:uint16 -> uint16
+
+val crc_ccitt_ffff    : input:bytes -> uint16
+
+val crc_ccitt_1d0f    : input:bytes -> uint16
+
+val crc_xmodem        : input:bytes -> uint16
