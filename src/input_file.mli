d613a797a359450901927cdf510f8fc5c99d2d5a
Darren Ldl <dldldev@yahoo.com>
2017-06-29 03:38:30 +1000

    Improved error handling of file_hash_and_split

--- src/input_file.mli
+++ src/input_file.mli
@@ -2,4 +2,4 @@ open Sbx_version
 
 type multihash_and_chunks  = bytes * (bytes list)
 
-val file_hash_and_split : ver:version -> filename:string -> multihash_and_chunks
+val file_hash_and_split : ver:version -> filename:string -> (multihash_and_chunks, string) result
