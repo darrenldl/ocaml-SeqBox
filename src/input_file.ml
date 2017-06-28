8dbc1ab2acb04fdb2de47ed6d5856565ae835b2e
Darren Ldl <dldldev@yahoo.com>
2017-06-28 21:07:40 +1000

    Removed debug output and commented out code

--- src/input_file.ml
+++ src/input_file.ml
@@ -12,19 +12,8 @@ let file_hash_and_split ~(ver:version) ~(filename:string) : multihash_and_chunks
   (* let buf             : bytes = Bytes.create read_block_size in *)
 
   let rec acc_chunks ~(acc:hash_state_and_chunks) ~(pos:int) ~(file:In_channel.t) : hash_state_and_chunks =
-    print_endline "flag A";
     let (hash_state, chunks)       = acc in
-    print_endline "flag B";
-    (* let read                       = In_channel.really_input file ~buf ~pos ~len:read_block_size in
-    ignore (
-      match read with
-      | Some x -> Printf.printf "got something\n"
-      | None   -> Printf.printf "got nothing\n"
-    ); *)
     let read_count           : int = In_channel.input        file ~buf ~pos:0 ~len:read_block_size in
-    Printf.printf "read count : %d\n" read_count;
-    Printf.printf "buffer     : %s\n" (Hex.hexdump_s (Hex.of_string buf));
-    (* let read_count                 = if read_count > 0 then read_count - 1 else read_count in (* adjust read_count *) *)
     let new_chunk                  = Bytes.create read_count in
     String.blit ~src:buf ~src_pos:0 ~dst:new_chunk ~dst_pos:0 ~len:read_count; (* copy content from buffer to new chunk*)
     SHA256.feed hash_state (Cstruct.of_bytes new_chunk);                                (* update hash state *)
