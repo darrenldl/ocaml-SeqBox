dbc571e9f41cba1d683975ed5ee8f0d2a6944b89
Darren Ldl <dldldev@yahoo.com>
2017-06-30 16:10:29 +1000

    Added more conversion utility functions

--- src/conv_utils.ml
+++ src/conv_utils.ml
@@ -6,6 +6,30 @@ let uint64_to_bytes (v:uint64) : bytes =
   buf
 ;;
 
+let uint32_to_bytes (v:uint32) : bytes =
+  let buf = Bytes.create 4 in
+  Uint32.to_bytes_big_endian v buf 0;
+  buf
+;;
+
+let uint16_to_bytes (v:uint16) : bytes =
+  let buf = Bytes.create 2 in
+  Uint16.to_bytes_big_endian v buf 0;
+  buf
+;;
+
+let uint8_to_bytes (v:uint8) : bytes =
+  let buf = Bytes.create 1 in
+  Uint8.to_bytes_big_endian v buf 0;
+  buf
+;;
+
 let string_to_bytes (str:string) : bytes =
   Bytes.of_string str
 ;;
+
+let bytes_to_hex_string (data:bytes) : string =
+  let `Hex str = Hex.of_cstruct (Cstruct.of_bytes data) in
+  str
+;;
+
---
+++ src/conv_utils.mli
@@ -0,0 +1,13 @@
+open Stdint
+
+val uint64_to_bytes : uint64 -> bytes
+
+val uint32_to_bytes : uint32 -> bytes
+
+val uint16_to_bytes : uint16 -> bytes
+
+val uint8_to_bytes  : uint8  -> bytes
+
+val string_to_bytes : string -> bytes
+
+val bytes_to_hex_string : bytes -> string
