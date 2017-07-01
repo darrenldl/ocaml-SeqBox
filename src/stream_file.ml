8560d5adc61291e075bcf41ca6436ac6c726cd58
Darren Ldl <dldldev@yahoo.com>
2017-07-01 15:15:54 +1000

    Fixed potential leak of file descriptor

--- src/stream_file.ml
+++ src/stream_file.ml
@@ -10,11 +10,13 @@ module Stream = struct
   let process_in_out ~(in_filename:string) ~(out_filename:string) ~(processor:(In_channel.t -> Out_channel.t -> ('a, string) result)) : ('a, string) result =
     try
       let in_file  = In_channel.create  ~binary:true in_filename  in
-      let out_file = Out_channel.create ~binary:true out_filename in
-      protect ~f:(fun () -> processor in_file out_file)
+      protect ~f:(fun () ->
+          let out_file = Out_channel.create ~binary:true out_filename in
+          protect ~f:(fun () -> processor in_file out_file)
+            ~finally:(fun () ->
+                Out_channel.close out_file))
         ~finally:(fun () ->
-            In_channel.close  in_file;
-            Out_channel.close out_file)
+            In_channel.close in_file)
     with
     | _ -> Error (sprintf_failed_to_rw ~in_filename ~out_filename)
   ;;
