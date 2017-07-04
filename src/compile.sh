dfc69484289943f31dddbc99ca2dcf38c7300aad
Darren Ldl <dldldev@yahoo.com>
2017-07-04 17:59:53 +0800

    Added processor helper for finding metadata block

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # corebuild -cflags -safe-string -pkgs stdint,nocrypto,core,hex input_file.byte
-corebuild -pkgs stdint,nocrypto,nocrypto.unix,core,hex,angstrom encode.byte
+corebuild -pkgs stdint,nocrypto,nocrypto.unix,core,hex,angstrom decode.byte
--- src/decode.ml
+++ src/decode.ml
@@ -1,5 +1,6 @@
 (* Decoding workflow
  *  Go through entire file to scan for metadata block (seq_num = 0)
+ *  using block size of 512(largest common divisor for implementable version 1, 3. Version 2 cannot be implemented)
  *    if metadata block exists
  *      all other data blocks must follow the metadata block's :
  *        version
@@ -31,3 +32,42 @@
  *        leave it there even if it doesn't match
  *
  *)
+open Stdint
+open Sbx_version
+open Sbx_block
+open Stream_file
+
+module Processor = struct
+  let rec find_metadata_block_proc (in_file:Core.In_channel.t) : Block.t option =
+    let open Read_chunk in
+    let len = 512 in  (* largest common divisor for version 1, 3 block size *)
+    let {no_more_bytes; chunk} = read in_file ~len in
+    if Bytes.length chunk < 16 then
+      None  (* at the end of file and got nothing *)
+    else
+      let test_header_bytes = Misc_utils.get_bytes chunk ~pos:0 ~len:16 in
+      let test_header : Header.raw_header option =
+        try
+          Some (Header.of_bytes test_header_bytes)
+        with
+        | Header.Invalid_bytes -> None in
+      match test_header with
+      | None        ->
+        find_metadata_block_proc in_file (* go to next block *)
+      | Some raw_header ->
+        let test_block : Block.t option =
+          if raw_header.seq_num = (Uint32.of_int 0) then
+            (* may have got a metadata block *)
+            try
+              Some (Block.of_bytes ~raw_header chunk)
+            with
+            | Block.Invalid_bytes -> None
+          else
+            None in
+        match test_block with
+        | None -> 
+          find_metadata_block_proc in_file (* go to next block *)
+        | Some block ->
+          Some block  (* found a valid block *)
+  ;;
+end
