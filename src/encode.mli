5125b77507bfae7d6b152faba1c878a61364de81
Darren Ldl <dldldev@yahoo.com>
2017-07-04 09:46:08 +0800

    Added note about possible side effect(printfs)

--- src/encode.mli
+++ src/encode.mli
@@ -6,5 +6,6 @@ module Processor : sig
   type stats = { blocks_written : int
                }
 
+  (* Resulting encoder may do Printf.printf to report progress etc *)
   val make_in_out_encoder : common:Header.common_fields -> metadata:(Metadata.t list) option -> stats Stream.in_out_processor
 end
