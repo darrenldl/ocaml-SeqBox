d832cd6e4fba7e729ef3508a6d29bd3e00de872f
Darren Ldl <dldldev@yahoo.com>
2017-07-04 10:50:54 +0800

    Drafted workflow for decoding

--- src/decode.ml
+++ src/decode.ml
@@ -11,10 +11,23 @@
  *        uid
  *
  *  when writing the blocks
- *    if CRC-CCITT fails, drop the block
+ *    if CRC-CCITT fails
+ *      drop the block
+ *      do a printf
+ *    else
+ *      check if block of that seq_num has been written before via Hashtlbl (seq_num:uint32 -> unit)
+ *        if no such block has been written yet
+ *          write the block
+ *        else
+ *          drop the block
+ *          do a printf
  *
  *  after writing all blocks
  *    if metadata block exists
  *      if outputfile exceeds file size
  *        use the file size recorded to trim the output file
+ *      do a hash on the decoded file
+ *        report if matches with the recorded hash if any
+ *        leave it there even if it doesn't match
+ *
  *)
