2224e1ac782839991787a85da03e9546074c341f
Darren Ldl <dldldev@yahoo.com>
2017-07-06 23:34:47 +0800

    Added benchmark results

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # corebuild -cflags -safe-string -pkgs stdint,nocrypto,core,hex input_file.byte
 corebuild -pkgs stdint,nocrypto,nocrypto.unix,core,hex,angstrom encode.native
+corebuild -pkgs stdint,nocrypto,nocrypto.unix,core,hex,angstrom decode.native
+corebuild -pkgs stdint,nocrypto,nocrypto.unix,core,hex,angstrom single_block_encode.native
+corebuild -pkgs stdint,nocrypto,nocrypto.unix,core,hex,angstrom single_block_decode.native
