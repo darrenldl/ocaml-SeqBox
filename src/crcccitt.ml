d4c1a201aa7507e6627501a2b81c3d6e76d00ff9
Darren Ldl <dldldev@yahoo.com>
2017-07-07 00:48:37 +0800

    2nd CRC optimization

--- src/crcccitt.ml
+++ src/crcccitt.ml
@@ -139,7 +139,7 @@ let update_crc_ccitt ~(crc:uint16) ~(single_byte:uint8) : uint16 =
 
 let crc_ccitt_generic ~(input:bytes) ~(start_val:uint16) : uint16 =
   let crc : uint16 ref = ref (Uint16.of_int 0) in
-  let val0x00FF        = Uint16.of_int 0x00FF  in
+  (* let val0x00FF        = Uint16.of_int 0x00FF  in *)
 
   crc := start_val;
 
@@ -148,14 +148,15 @@ let crc_ccitt_generic ~(input:bytes) ~(start_val:uint16) : uint16 =
            ^
            crc_tabccitt.(
              Uint16.to_int (
-               (
+               (* ( *)
                  (!crc >> 8)
                  ^ 
-                 let byte = (Uint8.of_bytes_big_endian input i) in
-                 Uint16.of_uint8 byte
-               )
+                 (* let byte = (Uint8.of_bytes_big_endian input i) in
+                 Uint16.of_uint8 byte *)
+                 Uint16.of_int (Char.code (Bytes.get input i))
+               (* )
                &
-               val0x00FF
+               val0x00FF *)
              )
            )
   done;
