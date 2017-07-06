82368373bef88fac4e68d8eb70f53e21fc42fcd4
Darren Ldl <dldldev@yahoo.com>
2017-07-07 01:50:51 +0800

    Changed Sbx_version to Sbx_specs

--- src/sbx_specs.ml
+++ src/sbx_specs.ml
@@ -1,15 +1,25 @@
 open Stdint
+open Misc_utils
 
 (* Only version 1 is supported as of time of writing *)
 type version = [ `V1 ]
 
-module Param_for_v1 = struct
-  let file_uid_len = 6
-  let signature    = Bytes.of_string "SBx"
-  let block_size   = 512
-  let header_size  = 16
-  let data_size    = block_size - header_size
+module Common_param = struct
+  let file_uid_len : int   = 6
+  let signature    : bytes = "SBx"
+  let header_size  : int   = 16
 end
+
+module Param_for_v1 = struct
+  let block_size   : int   = 512
+  let data_size    : int   = block_size - Common_param.header_size
+end
+
+let sbx_file_uid_len = Common_param.file_uid_len;;
+
+let sbx_signature    = Common_param.signature;;
+
+let sbx_header_size  = Common_param.header_size;;
 
 let ver_to_int          (ver:version) : int =
   match ver with
@@ -24,24 +34,15 @@ let ver_to_uint16       (ver:version) : uint16 =
   Uint16.of_int (ver_to_int ver)
 ;;
 
-let ver_to_file_uid_len (ver:version) : int =
-  match ver with
-  | `V1 -> Param_for_v1.file_uid_len
-;;
-
-let ver_to_signature    (ver:version) : bytes =
-  match ver with
-  | `V1 -> Param_for_v1.signature
+let ver_to_bytes        (ver:version) : bytes =
+  let buf = Bytes.make 2 '\x00' in
+  Uint16.to_bytes_big_endian (ver_to_uint16 ver) buf 0;
+  buf
 ;;
 
 let ver_to_block_size   (ver:version) : int =
   match ver with
   | `V1 -> Param_for_v1.block_size
-;;
-
-let ver_to_header_size  (ver:version) : int =
-  match ver with
-  | `V1 -> Param_for_v1.header_size
 ;;
 
 let ver_to_data_size    (ver:version) : int =
--- src/sbx_specs.mli
+++ src/sbx_specs.mli
@@ -3,11 +3,11 @@ open Stdint
 (* Only version 1 is supported as of time of writing *)
 type version = [ `V1 ]
 
-module Param_for_v1 : sig
-  val file_uid_len : int
-  val signature    : bytes
-  val block_size   : int
-end
+val sbx_file_uid_len    : int
+
+val sbx_signature       : bytes
+
+val sbx_header_size     : int
 
 val ver_to_int          : version -> int
 
@@ -15,12 +15,8 @@ val ver_to_uint8        : version -> uint8
 
 val ver_to_uint16       : version -> uint16
 
-val ver_to_file_uid_len : version -> int
-
-val ver_to_signature    : version -> bytes
+val ver_to_bytes        : version -> bytes
 
 val ver_to_block_size   : version -> int
 
-val ver_to_header_size  : version -> int
-  
 val ver_to_data_size    : version -> int
