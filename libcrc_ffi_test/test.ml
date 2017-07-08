d878e214345f484763edf93b43741c9fb6db99ef
Darren Ldl <dldldev@yahoo.com>
2017-07-08 14:41:42 +0800

    Updated crc printing code

--- libcrc_ffi_test/test.ml
+++ libcrc_ffi_test/test.ml
@@ -1,11 +1,13 @@
 open Stdint
+open Hex
 open Crcccitt_wrap
 
 let test () : unit =
   let res     = crc_ccitt_generic ~data:"abcd" ~start_val:(Uint16.of_int 0xFFFF) in
   let res_str = "  " in
   Uint16.to_bytes_big_endian res res_str 0;
-  Printf.printf "got : %s\n" res_str
+  let `Hex str = Hex.of_string res_str in
+  Printf.printf "got : %s\n" str
 ;;
 
 test ()
