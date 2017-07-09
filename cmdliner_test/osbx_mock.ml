b1e1067473743291e854a10c02763c05d8016a67
Darren Ldl <dldldev@yahoo.com>
2017-07-09 21:27:35 +0800

    Finished basic osbx mock design

--- cmdliner_test/osbx_mock.ml
+++ cmdliner_test/osbx_mock.ml
@@ -1,36 +1,41 @@
 open Cmdliner
 
-let encode in_file out_file =
-  Printf.printf "in : %s, out : %s\n" in_file out_file
+let help_secs = [
+ `S Manpage.s_common_options;
+ `P "These options are common to all commands.";
+ `S "MORE HELP";
+ `P "Use `$(mname) $(i,COMMAND) --help' for help on a single command.";`Noblank;
+ `P "Use `$(mname) help patterns' for help on patch matching."; `Noblank;
+ `P "Use `$(mname) help environment' for help on environment variables.";
+ `S Manpage.s_bugs; `P "Check bug reports at http://bugs.example.org.";]
 ;;
 
-let in_file =
-  let doc = "File to encode" in
-  Arg.(required & pos 0 (some file) None & info [] ~docv:"INFILE" ~doc)
-;;
-
-let out_file =
-  let doc = "Sbx container name" in
-  Arg.(required & pos 0 (some string) None & info [] ~docv:"OUTFILE" ~doc)
-;;
-
-let encode_t = Term.(const encode $ in_file $ out_file);;
-
-let encode_info =
+let encode_cmd =
+  let encode in_file out_file =
+    let out_file =
+      if (String.length out_file) = 0 then
+        in_file
+      else
+        out_file in
+    Printf.printf "in : %s, out : %s\n" in_file out_file in
+  let in_file =
+    let doc = "File to encode" in
+    Arg.(required & pos 0 (some file) None & info [] ~docv:"INFILE" ~doc) in
+  let out_file =
+    let doc = "Sbx container name" in
+    Arg.(value & pos 1 string "" & info [] ~docv:"OUTFILE" ~doc) in
   let doc = "encode files" in
+  Term.(const encode $ in_file $ out_file),
   Term.info "encode" ~doc
 ;;
 
-let main = Term.(const ());;
-
-let info =
-  let doc = "ocaml-SeqBox" in
-  let man = [ `S Manpage.s_authors
-            ; `P "Darren Ldl"
-            ; `S Manpage.s_bugs
-            ; `P "Open issues at ocaml-SeqBox github repo"
-            ] in
-  Term.info "osbx" ~doc ~exits:Term.default_exits ~man
+let default_cmd =
+  let doc = "a revision control system" in
+  let sdocs = Manpage.s_common_options in
+  let exits = Term.default_exits in
+  let man = help_secs in
+  Term.(ret (const (`Help (`Pager, None)))),
+  Term.info "osbx" ~version:"v1.0.0" ~doc ~sdocs ~exits ~man
 ;;
 
-let () = Term.exit @@ Term.eval_choice (main, info) [(encode_t, encode_info)]
+let () = Term.exit @@ Term.eval_choice default_cmd [encode_cmd]
