337a3e22d51da770c7ac845b1c51efbfb3040572
Darren Ldl <dldldev@yahoo.com>
2017-07-12 09:10:55 +0800

    Removed cmdliner_test directory

--- cmdliner_test/README.md
+++
@@ -1,1 +0,0 @@
-All test code are from the example code given at here : http://erratique.ch/software/cmdliner/doc/Cmdliner
--- cmdliner_test/revolt.ml
+++
@@ -1,7 +0,0 @@
-open Cmdliner
-
-let revolt () = print_endline "Revolt!";;
-
-let revolt_t = Term.(const revolt $ const ())
-
-let () = Term.exit @@ Term.eval (revolt_t, Term.info "revolt")
