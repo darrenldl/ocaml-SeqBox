02c2a9f6258e9ac9cad240598058cbfab597369b
Darren Ldl <darrenldldev@gmail.com>
2017-07-19 12:31:59 +0800

    Fine tuned Param

--- src/param.ml
+++ src/param.ml
@@ -3,22 +3,22 @@ module Common = struct
 end
 
 module Encode = struct
-  let progress_report_interval = 0.1  (* update every 100ms *)
+  let progress_report_interval = 0.2  (* update every 200ms *)
 end
 
 module Decode = struct
   let ref_block_scan_alignment = Common.block_scan_alignment
-  let progress_report_interval = 0.1  (* update every 100ms *)
+  let progress_report_interval = 0.2  (* update every 200ms *)
   let failure_list_max_length  = 500L
 end
 
 module Rescue = struct
   let scan_alignment           = Common.block_scan_alignment
-  let progress_report_interval = 0.1  (* update every 100ms *)
+  let progress_report_interval = 0.2  (* update every 200ms *)
   let log_write_interval       = 0.1  (* write  every 100ms *)
 end
 
 module Show = struct
-  let progress_report_interval = 0.1  (* update every 100ms *)
+  let progress_report_interval = 0.2  (* update every 200ms *)
   let meta_list_max_length     = 100L
 end
