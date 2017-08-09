916a33b860e38b067cb34e021cc3b621d0ef9006
Darren Ldl <darrenldldev@gmail.com>
2017-08-09 16:04:26 +1000

    Updated compare script

--- tests/versus.sh
+++ tests/versus.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-echo "Generating UID"
-
 uid=$(cat /dev/urandom | tr -dc 0-9a-f | fold -w 12 | head -n 1)
 
-echo ""
+dd if=/dev/urandom of=dummy bs=2096 count=1 &>/dev/null
 
-dd if=/dev/urandom of=dummy bs=2096 count=1
+python3 SeqBox/sbxenc.py -uid $uid dummy dummy_official.sbx -o &> /dev/null
 
-echo "Generating sbx container using official SeqBox"
-
-python3 SeqBox/sbxenc.py -uid $uid dummy dummy_official.sbx -o
-
-echo ""
-
-echo "Generating sbx container using osbx"
-
-./osbx encode dummy dummy_osbx.sbx -f --uid $uid
-
-echo ""
-
-echo "Comparing the two containers"
+./osbx encode dummy dummy_osbx.sbx -f --uid $uid &> /dev/null
 
 cmp -i 512 dummy_official.sbx dummy_osbx.sbx
+
+if [[ $? == 0 ]]; then
+  echo "No mismatches found"
+else
+  echo "Mismatches detected"
+fi
