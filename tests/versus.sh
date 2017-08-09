03ed96620e31732b6a2dc91a02be180eca78ce57
Darren Ldl <darrenldldev@gmail.com>
2017-08-09 16:24:28 +1000

    Optimised versus.sh

--- tests/versus.sh
+++ tests/versus.sh
@@ -4,9 +4,13 @@ uid=$(cat /dev/urandom | tr -dc 0-9a-f | fold -w 12 | head -n 1)
 
 dd if=/dev/urandom of=dummy bs=2096 count=1 &>/dev/null
 
+rm dummy_official.sbx
+
 python3 SeqBox/sbxenc.py -uid $uid dummy dummy_official.sbx -o &> /dev/null
 
-./osbx encode dummy dummy_osbx.sbx -f --uid $uid &> /dev/null
+rm dummy_osbx.sbx
+
+./osbx encode dummy dummy_osbx.sbx -f --uid $uid -s 1 &> /dev/null
 
 cmp -i 512 dummy_official.sbx dummy_osbx.sbx
 
