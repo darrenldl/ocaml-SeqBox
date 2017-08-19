ed927325e19e4eced64c11dc06e32c3694526f5c
Darren Ldl <darrenldldev@gmail.com>
2017-08-20 01:52:30 +1000

    Added documentation

--- src/sbx_block.mli
+++ src/sbx_block.mli
@@ -19,8 +19,10 @@ module Header : sig
 
   val common_fields_to_ver : common_fields -> version
 
+  (* raises Invalid_uid_length if uid length does not match the specs *)
   val make_common_fields   : ?uid:bytes    -> version    -> common_fields
 
+  (* raises Invalid_bytes if bytes are invalid *)
   val of_bytes             : bytes         -> raw_header
 
   val raw_header_is_meta   : raw_header    -> bool
@@ -54,12 +56,30 @@ module Block : sig
 
   type block_type = [ `Meta | `Data | `Any ]
 
+  (* raises Metadata.Too_much_data if generated metadata bytes are too long *)
   val make_metadata_block : Header.common_fields          -> fields:Metadata.t list -> t
 
+  (* raises
+   *    Invalid_seq_num if provided seq_num is invalid
+   *    Too_much_data   if provided data is too long
+   *)
   val make_data_block     : ?seq_num:uint32               -> Header.common_fields   -> data:bytes -> t
 
+  (* raises
+   *    Header.Missing_alt_seq_num if all of the following conditions are met
+   *      - block is a data block
+   *      - block does not contain a sequence number
+   *      - alt_seq_num is not specified
+   *    Invalid_seq_num            if alt_seq_num is invalid
+   *)
   val to_bytes            : ?alt_seq_num:uint32           -> t                      -> bytes
 
+  (* raises
+   *    Header.Invalid_bytes   if header bytes are invalid
+   *    Metadata.Invalid_bytes if metadata bytes are invalid
+   *    Invalid_size           if size of data is invalid
+   *    Invalid_bytes          if CRC-CCITT of data does not match the one recorded in header
+   *)
   val of_bytes            : ?raw_header:Header.raw_header -> ?skipped_already:bool  -> bytes      -> t
 
   val block_to_ver        : t -> version
