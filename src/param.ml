fc9c1bfca7ee786f71711d989a29a1adc236a001
Darren Ldl <darrenldldev@gmail.com>
2017-08-23 19:53:22 +1000

    Undone scan mode stuff

--- src/param.ml
+++ src/param.ml
@@ -66,16 +66,6 @@ module Rescue = struct
   let log_write_interval       = 1.0  (* write  every 1000ms *)
 end
 
-module Scan = struct
-  let progress_report_interval = 0.3  (* update every  300ms *)
-
-  module Scan_progress = struct
-    let display_while_active    = [Progress_bar; Percentage; Current_rate_short; Time_used_short; Time_left_short]
-    let display_on_finish       = [Time_used_long; Average_rate_long]
-    let display_on_finish_early = [Time_used_long; Average_rate_long]
-  end
-end
-
 module Show = struct
   let progress_report_interval = 0.3  (* update every  300ms *)
 
