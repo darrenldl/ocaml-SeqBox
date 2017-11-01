481883dc340c665357e38e31543c63908bfc7768
Darren Ldl <darrenldldev@gmail.com>
2017-11-01 14:58:49 +1100

    Patched processor_components

--- src/processor_components.ml
+++ src/processor_components.ml
@@ -2,9 +2,9 @@ open Sbx_block
 open Stream_file
 open Sbx_specs
 
-let bytes_to_block ?(raw_header:Header.raw_header option) (chunk:bytes) : Block.t option =
+let string_to_block ?(raw_header:Header.raw_header option) (chunk:string) : Block.t option =
   try
-    Some (Block.of_bytes ?raw_header chunk)
+    Some (Block.of_string ?raw_header chunk)
   with
   | Header.Invalid_bytes
   | Metadata.Invalid_bytes
@@ -12,19 +12,19 @@ let bytes_to_block ?(raw_header:Header.raw_header option) (chunk:bytes) : Block.
   | Block.Invalid_size     -> None
 ;;
 
-let patch_block_bytes_if_needed (in_file:in_channel) ~(raw_header:Header.raw_header) ~(chunk:bytes) : bytes =
+let patch_block_bytes_if_needed (in_file:in_channel) ~(raw_header:Header.raw_header) ~(chunk:string) : string =
   let open Read_chunk in
   let ideal_len   = ver_to_block_size raw_header.version in
-  let missing_len = ideal_len - (Bytes.length chunk) in
+  let missing_len = ideal_len - (String.length chunk) in
   if missing_len > 0 then
     match read in_file ~len:missing_len with
     | None                           -> chunk (* can't do anything, just give back the original piece *)
-    | Some { chunk = missing_chunk } -> Bytes.concat "" [chunk; missing_chunk]
+    | Some { chunk = missing_chunk } -> String.concat "" [chunk; missing_chunk]
   else
     chunk
 ;;
 
-let try_get_block_and_bytes_from_in_channel ?(fixed_ver:Sbx_specs.version option) ?(raw_header_pred:(Header.raw_header -> bool) = (fun _ -> true)) (in_file:in_channel) : int64 * ((Block.t * bytes) option) =
+let try_get_block_and_bytes_from_in_channel ?(fixed_ver:Sbx_specs.version option) ?(raw_header_pred:(Header.raw_header -> bool) = (fun _ -> true)) (in_file:in_channel) : int64 * ((Block.t * string) option) =
   let open Read_chunk in
   let len = 
     match fixed_ver with
@@ -33,18 +33,18 @@ let try_get_block_and_bytes_from_in_channel ?(fixed_ver:Sbx_specs.version option
   match read in_file ~len with
   | None           -> (0L, None)
   | Some { chunk } ->
-    let read_len : int64 = Int64.of_int (Bytes.length chunk) in
+    let read_len : int64 = Int64.of_int (String.length chunk) in
     try
-      let raw_header_bytes : bytes             = Misc_utils.get_bytes chunk ~pos:0 ~len:16 in
-      let raw_header       : Header.raw_header = Header.of_bytes raw_header_bytes in
+      let raw_header_string : string            = Misc_utils.get_sub_string chunk ~pos:0 ~len:16 in
+      let raw_header        : Header.raw_header = Header.of_string raw_header_string in
       if raw_header_pred raw_header then
-        let chunk            : bytes                    =
+        let chunk            : string                   =
           match fixed_ver with
           | Some _ -> chunk
           | None   -> patch_block_bytes_if_needed in_file ~raw_header ~chunk in
-        let read_len         : int64                    = Int64.of_int (Bytes.length chunk) in
-        let block            : Block.t option           = bytes_to_block ~raw_header chunk in
-        let block_and_bytes  : (Block.t * bytes) option =
+        let read_len         : int64                    = Int64.of_int (String.length chunk) in
+        let block            : Block.t option           = string_to_block ~raw_header chunk in
+        let block_and_bytes  : (Block.t * string) option =
           match block with
           | None   -> None
           | Some b -> Some (b, chunk) in
